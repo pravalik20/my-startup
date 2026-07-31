6212695605a39f6c14b0d5c21017450bc85282cb
Pravalik <pravalik@Pravaliks-MacBook-Air.local>
2026-07-31 16:15:44 +0530

    fixed mobile hero layout and developer model

--- src/components/Hero/DeveloperModel.jsx
+++ src/components/Hero/DeveloperModel.jsx
@@ -1,64 +1,31 @@
 "use client";
 
+import Image from "next/image";
 import { motion } from "framer-motion";
 
 export default function DeveloperModel() {
-
   return (
-
     <motion.div
-
-      initial={{
-        opacity:0,
-        y:40
-      }}
-
+      initial={{ opacity: 0, y: 40 }}
       animate={{
-        opacity:1,
-        y:[0,-10,0]
+        opacity: 1,
+        y: [0, -10, 0],
       }}
-
       transition={{
-        opacity:{
-          duration:1
+        opacity: { duration: 1 },
+        y: {
+          duration: 4,
+          repeat: Infinity,
+          ease: "easeInOut",
         },
-
-        y:{
-          duration:4,
-          repeat:Infinity,
-          ease:"easeInOut"
-        }
       }}
-
-      className="
-      relative
-      z-30
-      flex
-      justify-center
-      w-full
-      "
-
+      className="relative z-30"
     >
-    
-      <img
-
-        src="/models/developer.png"
-
-        alt="Developer"
-
-        className="
-        w-[280px]
-        sm:w-[400px]
-        md:w-[550px]
-        xl:w-[650px]
-        h-auto
-        object-contain
-        "
-
-      />
-
+<img
+ src="/models/developer.png"
+ alt="Developer"
+ width={650}
+/>
     </motion.div>
-
   );
-
 }
